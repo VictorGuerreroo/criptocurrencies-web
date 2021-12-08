0fc79dc4490fa6f27b6b1a6080cc278e0e9e1906
Victor <v.g.m_@hotmail.com>
2021-12-08 20:08:41 +0100

    Created filters file

---
+++ src/filters.ts
@@ -0,0 +1,10 @@
+import numeral from 'numeral'
+
+const dollarFilter = function(value : number) {
+    if(!value) {
+        return '$ 0'
+    }
+    return numeral(value).format('$0,0.00')
+}
+
+export { dollarFilter}
