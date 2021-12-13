9421eefe526e394b6b27244a654431628bd0ff4f
Victor <v.g.m_@hotmail.com>
2021-12-13 21:19:35 +0100

    WIP

---
+++ src/components/Footer.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-footer
+        color="primary"
+        padless
+        dark
+    >
+        <v-row justify="center" no-gutters>
+            <!-- <v-btn
+                v-for="link in links"
+                :key="link"
+                color="white"
+                text
+                rounded
+                class="my-2"
+            >
+                {{ link }}
+            </v-btn> -->
+            <v-col class="black py-4 text-center white--text" cols="12">
+
+            {{ new Date().getFullYear() }} — <strong>Víctor Guerrero</strong>
+
+            </v-col>
+        </v-row>
+    </v-footer>
+</template>
+
+<script lang="ts">
+
+import Vue from 'vue'
+
+export default Vue.extend ({
+    name:'footerComponent',
+
+    data: () => ({
+        links: [
+            'Home',
+            'About Us',
+            'Team',
+            'Services',
+            'Blog',
+            'Contact Us',
+            ],
+        }),
+    })
+</script>
+
+<style>
+
+</style>
